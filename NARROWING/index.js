4f5dd313cc2ca4be194baaca581401b2d1255ae6
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-08-02 12:25:32 -0300

    narrowing, type guard and checking of values

--- NARROWING/index.js
+++ NARROWING/index.js
@@ -1,111 +1,34 @@
 "use strict";
-let numbers = [1, 2, 3];
-numbers.push(5);
-console.log(numbers);
-const nomes = ['matheus', 'joão'];
-//outra sintaxe array
-const nums = [100, 200];
-nums.push(300);
-console.log(nums);
-//3 any
-const arr = [1, 'teste', 'hello', []];
-arr.push(true);
-console.log(arr);
-//tipo de parâmetro de funções
-function Apresentation(name, age) {
-    console.log(`Meu nome é ${name}\nidade: ${age}`);
-}
-Apresentation('gabriel', 20);
-//tipo de retorno
-function saudacao(name) {
-    return `Olá ${name}`;
-}
-console.log(saudacao('gabriel'));
-//funções anônimas
-setTimeout(() => {
-    const sallary = 1000;
-    //console.log(parseFloat(sallary))
-    //console.log(sallary)
-}, 2000);
-// 7 - tipos de objeto
-function passCoordinates(coord) {
-    console.log('X coordinates: ' + coord.x);
-    console.log('Y coordinates: ' + coord.y);
-}
-const objCoord = { x: 329, y: 84.9 };
-passCoordinates(objCoord);
-//passCoordinates({y: 12, y: 5})
-//8 - opcionais
-function showNumbers(a, b, c) {
-    console.log('a: ' + a);
-    console.log('b: ' + b);
-    console.log('c: ' + b);
-}
-showNumbers(2, 3, 4);
-//9 validando argumento opcional
-function advancedGreeting(firstName, lastName) {
-    if (lastName !== undefined) {
-        return `Olá, ${firstName} ${lastName}, tudo bem?`;
+function sum(a, b) {
+    if (typeof a === 'string' && typeof b === 'string') {
+        console.log(parseFloat(a) + parseFloat(b));
     }
-    return `Olá, ${firstName}, tudo bem?`;
-}
-console.log(advancedGreeting('gabriel', 'victor'));
-console.log(advancedGreeting('gabriel'));
-//10 - union types
-function showBalance(balance) {
-    return `O saldo da conta é R$${balance}`;
-}
-console.log(showBalance(500));
-console.log(showBalance('500'));
-const Arr = [10, 'gabriel', true];
-// 11 - avançando em union types
-function showUserRole(role) {
-    if (typeof role === 'boolean') {
-        return "Usuário não aprovado!";
+    else if (typeof a === 'number' && typeof b === 'number') {
+        console.log(a + b);
     }
     else {
-        return `a função do usuário é ${role}`;
+        console.log('Impossível realizar a operação');
     }
 }
-console.log(showUserRole(false));
-console.log(showUserRole('programador'));
-function showId(id) {
-    console.log(`O id é: ${id}`);
+sum('2', '3');
+sum(2, 5);
+//2 checando se o valor existe
+function operations(arr, operation) {
+    if (operation) {
+        if (operation === 'sum') {
+            const sum = arr.reduce((i, total) => i + total);
+            console.log(sum);
+        }
+        else if (operation === 'multiply') {
+            const mult = arr.reduce((i, total) => i * total);
+            console.log(mult);
+        }
+        else {
+        }
+    }
+    else {
+        console.log('Por favor, defina uma operação');
+    }
 }
-showId(1234);
-showId('24ask2');
-showId('123');
-function showCoords(obj) {
-    return `x: ${obj.x}, y: ${obj.y} e z: ${obj.z}`;
-}
-const coordObj = {
-    x: 10,
-    y: 5,
-    z: 25
-};
-console.log(showCoords(coordObj));
-const pessoa = {
-    name: 'gabriel',
-    age: 18
-};
-console.log(pessoa);
-//15 - literal types
-let test;
-test = 'testando';
-console.log(test);
-function showDirection(direction) {
-    console.log(`A direção é ${direction}`);
-}
-showDirection('left');
-//16 - non null assertion operators
-const p = document.getElementById('some-p');
-console.log(p.innerText);
-//17 - bigint
-let n;
-n = 1000n;
-console.log(n + 100n);
-//Symbol
-let symbolA = Symbol("a");
-let symbolB = Symbol("a");
-console.log(symbolA === symbolB);
-console.log(symbolA == symbolB);
+operations([1, 2, 3], 'sum');
+operations([1, 2, 3], 'multiply');
--- NARROWING/index.ts
+++ NARROWING/index.ts
@@ -1,167 +1,32 @@
-let numbers: number[] = [1, 2, 3]
-
-numbers.push(5)
-console.log(numbers)
-
-const nomes: string[] = ['matheus', 'joão']
-
-//outra sintaxe array
-
-const nums: Array<number> = [100, 200]
-
-nums.push(300)
-console.log(nums);
-
-//3 any
-
-const arr: any = [1, 'teste', 'hello', []]
-
-arr.push(true)
-console.log(arr)
-
-//tipo de parâmetro de funções
-function Apresentation(name: string, age: number): any {
-    console.log(`Meu nome é ${name}\nidade: ${age}`)
-}
-Apresentation('gabriel', 20)
-
-//tipo de retorno
-function saudacao(name: string): string {
-    return `Olá ${name}`
-}
-console.log(saudacao('gabriel'))
-
-//funções anônimas
-setTimeout(() => {
-    const sallary: number = 1000
-    //console.log(parseFloat(sallary))
-    //console.log(sallary)
-}, 2000);
-
-// 7 - tipos de objeto
-function passCoordinates(coord: { y: number, x: number }) {
-    console.log('X coordinates: ' + coord.x)
-    console.log('Y coordinates: ' + coord.y)
-}
-
-const objCoord = { x: 329, y: 84.9 }
-passCoordinates(objCoord)
-//passCoordinates({y: 12, y: 5})
-
-//8 - opcionais
-function showNumbers(a: number, b: number, c?: number) {
-    console.log('a: ' + a)
-    console.log('b: ' + b)
-    console.log('c: ' + b)
-}
-
-showNumbers(2, 3, 4)
-
-//9 validando argumento opcional
-
-function advancedGreeting(firstName?: string, lastName?: string) {
-    if (lastName !== undefined) {
-        return `Olá, ${firstName} ${lastName}, tudo bem?`
-    }
-    return `Olá, ${firstName}, tudo bem?`
-}
-console.log(advancedGreeting('gabriel', 'victor'))
-console.log(advancedGreeting('gabriel'))
-
-//10 - union types
-
-function showBalance(balance: string | number) {
-    return `O saldo da conta é R$${balance}`
-}
-console.log(showBalance(500))
-console.log(showBalance('500'))
-
-const Arr: Array<string | number | boolean> = [10, 'gabriel', true]
-
-// 11 - avançando em union types
-
-function showUserRole(role: boolean | string) {
-    if (typeof role === 'boolean') {
-        return "Usuário não aprovado!"
-    }
-    else {
-        return `a função do usuário é ${role}`
+type numbers = number | string
+function sum(a: numbers, b: numbers) {
+    if (typeof a === 'string' && typeof b === 'string') {
+        console.log(parseFloat(a) + parseFloat(b))
+    } else if (typeof a === 'number' && typeof b === 'number') {
+        console.log(a + b)
+    } else {
+        console.log('Impossível realizar a operação')
     }
 }
-console.log(showUserRole(false))
-console.log(showUserRole('programador'))
+sum('2', '3')
+sum(2, 5)
 
-// 12 - type alias
-type ID = string | number
-function showId(id: ID) {
-    console.log(`O id é: ${id}`)
+//2 checando se o valor existe
+function operations(arr: number[], operation: string | undefined) {
+    if (operation) {
+        if (operation === 'sum') {
+            const sum = arr.reduce((i, total) => i + total)
+            console.log(sum)
+        } else if (operation === 'multiply') {
+            const mult = arr.reduce((i, total) => i * total)
+            console.log(mult)
+        }
+        else {
+
+    }
+    } else {
+        console.log('Por favor, defina uma operação')
+    }
 }
-showId(1234)
-showId('24ask2')
-showId('123')
-
-//13 - interface
-
-interface Point {
-    x: number
-    y: number
-    z: number
-}
-function showCoords(obj: Point) {
-    return `x: ${obj.x}, y: ${obj.y} e z: ${obj.z}`
-}
-
-const coordObj: Point = {
-    x: 10,
-    y: 5,
-    z: 25
-}
-console.log(showCoords(coordObj))
-
-//14 - interface x alias type
-
-interface Person {
-    name: string
-}
-
-interface Person {
-    age: number
-}
-const pessoa: Person = {
-    name: 'gabriel',
-    age: 18
-}
-console.log(pessoa)
-
-type Another = {
-    first: number
-}
-
-//15 - literal types
-let test: 'testando'
-
-test = 'testando'
-console.log(test)
-
-function showDirection(direction: "left" | "right" | "center") {
-    console.log(`A direção é ${direction}`)
-}
-showDirection('left')
-
-//16 - non null assertion operators
-const p = document.getElementById('some-p')
-console.log(p!.innerText)
-
-//17 - bigint
-
-let n: bigint
-n = 1000n
-
-console.log(n + 100n)
-
-//Symbol
-let symbolA: symbol = Symbol("a")
-let symbolB: symbol = Symbol("a")
-
-console.log(symbolA === symbolB)
-console.log(symbolA == symbolB)
+operations([1, 2, 3], 'sum')
+operations([1, 2, 3], 'multiply')
