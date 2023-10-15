08a52b8855b8b7fd670f6e98703bddfafcdd1d0e
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-10-14 23:33:13 -0300

    finish from the express teory

--- 12_express_com_ts/src/app.ts
+++ 12_express_com_ts/src/app.ts
@@ -1,7 +1,7 @@
 //1 - iniciando o projeto
 
 //2 - inicializar o express
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express()
 
@@ -10,6 +10,12 @@ app.use(express.json())
 app.get('/', (req, res) => {
     return res.send("Hello express");
 })
+//11 - middlewares para todas as rotas
+function showPath(req: Request, res: Response, next: NextFunction) {
+    console.log(req.path);
+    next();
+}
+app.use(showPath)
 app.post('/api/product', (req, res) => {
     console.log(req.body);
 
@@ -64,6 +70,42 @@ app.get('/api/product/:id/review/:reviewId', (req: Request, res: Response) => {
 
     return res.send(`Acesssando a review ${id} com a review ${idReview}`)
 })
+//9 - router handler
+const getUser = (req: Request, res: Response) => {
+    console.log(`Resgatando o usuário com ID ${req.params.id}`);
+    return res.send('O usuário foi encontrado')
+}
+//10 - middleware
+function checkUser(req: Request, res: Response, next: NextFunction) {
+    if (req.params.id === '1') {
+        console.log('pode seguir');
+        next();
+    } else {
+        console.log("Acesso restrito");
+
+    }
+}
+app.get('/api/user/:id/acess', checkUser, (req: Request, res: Response) => {
+    return res.json({ msg: 'bem vindo a adm' })
+})
+app.get('/api/user/:id', getUser);
+
+//12 - req e res com generics
+app.get('/api/user/:id/details/:name', (req: Request<{ id: string, name: string }>, res: Response<{ status: boolean }>) => {
+    console.log(`id: ${req.params.id}`);
+    console.log(`name: ${req.params.name}`);
+
+    return res.json({ status: true })
+})
+//13 - tratando erros
+app.get('/api/error', (req: Request, res: Response) => {
+    try {
+        throw new Error("Algo deu errado");
+    } catch (e: any) {
+        res.status(500).json({ msg: e.message })
+    }
+})
+
 app.listen(3000, () => {
     console.log('aplicação de ts com express');
 })
